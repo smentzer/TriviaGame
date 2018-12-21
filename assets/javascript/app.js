d70fcc93cd083204b7e51f749ac3e8a6821af392
Cayde6 <stephaniementzer@gmail.com>
2018-12-21 17:11:03 -0500

    questions created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,106 @@
 // console.log("is this thing on?") (it was in fact on)
-//timer 
 var audio = new Audio("friday13th.mp3");
 var score = 0;
 
-setTimeout(timeUp, 15000); {
-    function timeUp() {
-        $("#time-left").append("<h2>Times up</h2>");
-        console.log("done")
+//count down timer
+var timer;
+var time = 50;
+
+timer = setInterval(function() {
+    time--;
+    $("#timer").html(time);
+
+    if (time === 0) {
+        clearInterval(timer); 
         audio.play();
-    };
-}
-    //questions 
-    var myQuestions = [
+        //call submit function
+     };
+
+}, 1000);
+   
+
+
+
+//questions 
+    var questions = [
         {
-            question: "If you are a teenager living on Elm Street what should you never do?",
-            answers: {
+            question: '-If you are a teenager living on Elm Street what should you never do?',
+            choices: {
                 a: 'Go to sleep',
                 b: 'Play with dolls',
-                c: 'Go to the prom'
+                c: 'Go to the prom',
             },
-            correctAnswer: "a"
+            answer: 'a'
         },
         {
-            question: "If you are looking for a job on Crystal Lake what offer should you not accept?",
-            answers: {
+            question: '-If you are looking for a job on Crystal Lake what offer should you not accept?',
+            choices: {
                 a: 'Mailman',
                 b: 'Camp counselor',
-                c: 'TA'
+                c: 'TA',
             },
-            correctAnswer: "b"
+            answer: 'b'
         },
         {
-            question: "All work and no play makes...?",
-            answers: {
+            question: '-All work and no play makes...?',
+            choices: {
                 a: 'right',
-                b: 'heres Johnny!',
-                c: 'Jack a dull boy'
+                b: 'here\'\s Johnny!',
+                c: 'Jack a dull boy',
             },
-            correctAnswer: "c"
+            answer: 'c'
         },
         {
-            question: "The calls are coming from...",
-            answers: {
+            question: '-The calls are coming from...',
+            choices: {
                 a: 'Texas',
                 b: 'inside the house',
-                c: 'the neighbors'
+                c: 'the neighbors',
             },
-            correctAnswer: "b"
+            answer: 'b'
         },
         {
-            question: "Who is the killer in Halloween?",
-            answers: {
+            question: '-Who is the killer in Halloween?',
+            choices: {
                 a: 'Jason Vorhees',
                 b: 'Norman Bates',
-                c: 'Michael Myers'
+                c: 'Michael Myers',
             },
-            correctAnswer: "c"
+            answer: 'c'
         },
 
-    ]
-    
+    ];
+    function startGame() {
+        for (var i = 0; i < questions.length; i++) {
+            $("#quiz").append(questions[i].question + "<br>");
+            $("#quiz").append("<input type='radio' name'" +i+ "'value='a'>" + questions[i].choices.a + "<br>");
+            $("#quiz").append("<input type='radio' name'" +i+ "'value='b'>" + questions[i].choices.b + "<br>");
+            $("#quiz").append("<input type='radio' name'" +i+ "'value='c'>" + questions[i].choices.c + "<br>");
+        }
+    }
+
+    startGame();
+//collect user input
+var userChoice;
+var correctAnswer;
+var questionsRight;
+var questionsWrong;
+var incompleteAnswers;
+
+
+function submit() {
+if (userChoice !== correctAnswer) {
+    questionsWtong++;
+    $("#").text(questionsWrong);
+}
+else if (userChoice === correctAnswer){
+    questionsRight++;
+    $("#").text(questionsRight);
+}
+};
+
+
+
+
+
+
